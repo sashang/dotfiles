36741189439b955b141b3017ae853182245f6f8d
sashan <sashan@zenskg.net>
2014-10-27 10:40:09 +0100

    spawn xmobar on startup.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,8 +1,17 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import System.IO
 
-main = xmonad defaultConfig
-  { terminal = "gnome-terminal"
-  , modMask = mod4Mask
-  , borderWidth = 3
-  }
+
+main = do
+  xmproc <- spawnPipe "/home/sashan/bin/xmobar /home/sashan/.xmobar"
+  xmonad $ defaultConfig
+      { manageHook = manageDocks <+> manageHook defaultConfig
+      , layoutHook = avoidStruts $ layoutHook defaultConfig
+      , terminal = "gnome-terminal"
+      , modMask = mod4Mask
+      , borderWidth = 3
+      }
 
