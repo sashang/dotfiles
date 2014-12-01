19614d0af6bee5c8c26af94f9bb47089b8a17af5
sashan <sashan@zenskg.net>
2014-12-01 14:12:37 +1100

    Correct symlinks

--- .arch-custom-install.sh
+++ .arch-custom-install.sh
@@ -7,14 +7,15 @@ systemctl restart syslog-ng.service
 sudo pacman -S gcc ghc
 sudo pacman -S xorg xorg-xinit xorg-xclock xorg-twm xmonad dmenu
 sudo pacman -S cabal-install
+cabal update
 cabal install xmobar
 sudo pacman -S gvim tmux
 sudo pacman -S firefox mutt pwsafe
 sudo pacman -S ttf-inconsolata ttf-dejavu
 mkdir -p $HOME/code/dotfiles
 mkdir -p $HOME/.vim/bundle
-ln -sf $HOME/.cabal/bin/xmobar $HOME/bin/xmobar
-ln -sf $HOME/.xmobar $HOME/code/dotfiles/.xmobar
-ln -sf $HOME/.xmonad/xmonad.hs $HOME/code/dotfiles/xmonad/xmonad.hs
+ln -sf $HOME/bin/xmobar $HOME/.cabal/bin/xmobar
+ln -sf $HOME/code/dotfiles/.xmobar $HOME/.xmobar 
+ln -sf $HOME/code/dotfiles/xmonad/xmonad.hs $HOME/.xmonad/xmonad.hs 
 xmonad --recompile
 
