3434aa697cfe670d1d3bb609a42d2427eba8b2cf
sashan <sashan@zenskg.net>
2014-12-02 08:29:18 +1100

    updated to Melbourne weather station.

--- .arch-custom-install.sh
+++ .arch-custom-install.sh
@@ -4,18 +4,22 @@ sudo pacman -Syu
 sudo pacman -S syslog-ng
 systemctl enable syslog-ng.service
 systemctl restart syslog-ng.service
-sudo pacman -S gcc ghc
-sudo pacman -S xorg xorg-xinit xorg-xclock xorg-twm xmonad dmenu xmonad-contrib
-sudo pacman -S cabal-install
+sudo pacman --noconfirm -S gcc ghc
+sudo pacman --noconfirm -S xorg xorg-xinit xorg-xclock xorg-twm xmonad dmenu xmonad-contrib
+sudo pacman --noconfirm -S cabal-install
 cabal update
 cabal install xmobar
-sudo pacman -S gvim tmux
-sudo pacman -S firefox mutt pwsafe
-sudo pacman -S ttf-inconsolata ttf-dejavu
+sudo pacman --noconfirm -S gvim tmux
+sudo pacman --noconfirm -S firefox mutt pwsafe
+sudo pacman --noconfirm -S ttf-inconsolata ttf-dejavu
 mkdir -p $HOME/code/dotfiles
 mkdir -p $HOME/.vim/bundle
 ln -sf $HOME/bin/xmobar $HOME/.cabal/bin/xmobar
 ln -sf $HOME/code/dotfiles/.xmobar $HOME/.xmobar 
 ln -sf $HOME/code/dotfiles/xmonad/xmonad.hs $HOME/.xmonad/xmonad.hs 
 xmonad --recompile
+cat << EOF > $HOME/.xinitrc
+xsetroot -cursor_name left_ptr
+exec xmonad
+EOF
 
