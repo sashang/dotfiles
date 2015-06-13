ea3a284f965e325eaf6161400f7dbb3d6b3e9f82
sashan <sashan@zenskg.net>
2015-06-14 01:11:26 +1000

    update

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -4,7 +4,9 @@ function fish_prompt
         set branch (git branch ^ /dev/null | grep \* |  cut -d " " -f 2)
     end
     if test $err = 0
-        printf '%s@%s %s%s %s%s %s> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color purple) $branch (set_color normal)
+        printf '%s@%s %s%s %s%s %s> ' (whoami) (hostname|cut -d . -f 1) \
+            (set_color $fish_color_cwd) (prompt_pwd) (set_color purple) \
+            $branch (set_color normal)
     else
         printf '%s@%s %s%s %s%s %s%s %s> ' (whoami) (hostname|cut -d . -f 1) \
             (set_color $fish_color_cwd) (prompt_pwd) (set_color purple) $branch \
