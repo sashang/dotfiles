9ea8f9ce9656f27aa3e64b59a8d474efa9fbd994
sashan <sashang@gmail.com>
2015-06-16 13:23:05 +1000

    Update the install script.

--- .arch-custom-install.sh
+++ .arch-custom-install.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
+## A pacman based 1st time personal environment setup script
 
+package_list="gcc gnome firefox mutt pwsafe ttf-inconsolata ttf-dejavu gvim tmux"
 sudo pacman -Syu
 sudo pacman -S syslog-ng
 systemctl enable syslog-ng.service
 systemctl restart syslog-ng.service
-sudo pacman --noconfirm -S gcc ghc
-sudo pacman --noconfirm -S xorg xorg-xinit xorg-xclock xorg-twm xmonad dmenu xmonad-contrib
-sudo pacman --noconfirm -S cabal-install
-cabal update
-cabal install xmobar
-sudo pacman --noconfirm -S gvim tmux
-sudo pacman --noconfirm -S firefox mutt pwsafe
-sudo pacman --noconfirm -S ttf-inconsolata ttf-dejavu
-mkdir -p $HOME/code/dotfiles
-mkdir -p $HOME/.vim/bundle
-ln -sf $HOME/bin/xmobar $HOME/.cabal/bin/xmobar
-ln -sf $HOME/code/dotfiles/.xmobar $HOME/.xmobar 
-ln -sf $HOME/code/dotfiles/xmonad/xmonad.hs $HOME/.xmonad/xmonad.hs 
-xmonad --recompile
-cat << EOF > $HOME/.xinitrc
-xsetroot -cursor_name left_ptr
-exec xmonad
-EOF
+sudo pacman --noconfirm -S $package_list
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#create directoies.
+[ -z "$HOME/.vim/bundle" ] || mkdir -p "$HOME/.vim/bundle"
+[ -z "$HOME/.config/fish/functions" ] || mkdir -p "$HOME/.config/fish/functions"
+[ -z "$HOME/.mutt/" ] || mkdir -p "$HOME/.config"
+#setup symlinks
+ln -sf "$HOME/code/dotfiles/.vimrc" "$HOME/.vimrc"
+ln -sf "$HOME/code/dotfiles/.bashrc" "$HOME/.bashrc"
+ln -sf "$HOME/code/dotfiles/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$HOME/code/dotfiles/.mutt/muttrc" "$HOME/.mutt/muttrc"
+ln -sf "$HOME/code/dotfiles/.mutt/theme" "$HOME/.mutt/theme"
 
