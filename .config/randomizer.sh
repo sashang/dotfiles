951f91cb301bffc4abadf6cbf31f106be101dde8
sashan <sashang@gmail.com>
2015-07-12 17:59:16 +1000

    add more colors for urxvt

--- .config/randomizer.sh
+++ .config/randomizer.sh
@@ -2,9 +2,10 @@
 
 ## picks a random terminal color scheme and tells X to use it
 
-colors=($(ls *))
+colors=($(ls $HOME/.config/xcolors/*))
 len=${#colors[@]}
 idx=$(($RANDOM % $len))
 xrdb -override ${colors[$idx]}
+echo ${colors[$idx]}
 urxvt &
 
