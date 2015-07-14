5b475e4e7eb830a9bd481563c5f2cb2eb1c4b710
sashan <sashang@gmail.com>
2015-07-14 10:04:10 +1000

    Use shuf instead of $RANDOM

--- randomizer.sh
+++ randomizer.sh
@@ -2,10 +2,7 @@
 
 ## picks a random terminal color scheme and tells X to use it
 
-colors=($(ls $HOME/.config/xcolors/*))
-len=${#colors[@]}
-idx=$(($RANDOM % $len))
-xrdb -merge ${colors[$idx]}
-echo ${colors[$idx]}
+colors=($(find $HOME/.config/xcolors -follow -type f | shuf | head -n 1))
+xrdb -merge ${colors}
 urxvt &
 
