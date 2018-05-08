22da0df8c1de77a5c9f0485dc9ff106aa294bca0
sashang@gmail.com <sashang@gmail.com>
2018-05-08 15:44:24 +1000

    Remove opam stuff.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,6 @@
-set -x PATH "$HOME/.npm-packages/bin" "$HOME/bin" "$HOME/.gem/ruby/2.4.0/bin" $PATH
+set -x PATH "$HOME/.npm-packages/bin" "$HOME/bin" $PATH
 set -x EDITOR vim
 set -x CC "ccache gcc"
 set -x CXX "ccache g++"
 
 source $HOME/.local.fish
-
-# OPAM configuration
-. /home/sashan/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
