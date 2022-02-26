44e1bed8cb098acf93aa444ef14dcf0ed26b52bd
sashan <sashan@gmail.com>
2022-02-26 21:40:54 +1100

    remove CC and CXX

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,4 @@
 set -x PATH "$HOME/.npm-packages/bin" "$HOME/.gem/ruby/2.7.0/bin" "$HOME/.gem/ruby/2.6.0/bin" "$HOME/bin" $PATH
-set -x EDITOR vim
-set -x CC "ccache gcc"
-set -x CXX "ccache g++"
+set -x EDITOR nvim
 
 source $HOME/.local.fish
