973a91ced4640280eab2ee792711624185e4ca9d
sashan <sashang@gmail.com>
2024-07-27 23:36:25 +1000

    Update fsharp config
    Works with project files now.
    Not sure why the unused* settings don't work. I don't need to see them
    in the buffer.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
-set -x PATH "$HOME/.cargo/bin" "$HOME/.npm-packages/bin" "$HOME/.gem/ruby/2.7.0/bin" "$HOME/bin" $PATH
+set -x PATH "$HOME/.cargo/bin" "$HOME/.npm-packages/bin" "$HOME/code/dotnet" "$HOME/bin" $PATH
 set -x EDITOR nvim
 
 source $HOME/.local.fish
