dbcbc6d5f1e114de5652f2db5d9431ea924ec4b2
sashan <sashang@gmail.com>
2024-07-30 20:40:58 +1000

    Changes for fsautocomplete

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,25 +1,41 @@
 return {
     "neovim/nvim-lspconfig",
     config = function()
-        local lspconfig = require("lspconfig")
+        local lspconfig = require('lspconfig')
         local on_attach = lspconfig.on_attach
         local capabilities = lspconfig.capabilities
         lspconfig.rust_analyzer.setup({})
         lspconfig.lua_ls.setup({})
-        lspconfig.fsautocomplete.setup({
+        lspconfig.fsautocomplete.setup{
             root_dir = lspconfig.util.root_pattern('*.fsproj', '*.sln', '.git'),
             cmd = { 'fsautocomplete', '--adaptive-lsp-server-enabled' },
             filetypes = { 'fsharp' },
             init_options = {
-                AutomaticWorkspaceInit = true,
+                AutomaticWorkspaceInit = false,
             },
             settings = {
                 FSharp = {
                     UnusedDeclarationsAnalyzer = false,
                     UnusedOpensAnalyzer = false,
+                    EnableReferenceCodelens = false,
+                    excludeProjectDirectories = {
+                        ".git",
+                        "paket-files",
+                        ".paket",
+                        ".github",
+                        ".idea",
+                        "obj",
+                        "bin",
+                        "deploy",
+                        "dist",
+                        "node_modules",
+                        ".vscode",
+                        "vsintegration"
+                    },
+                    enableAdaptiveLspServer = true
                 },
             },
-        })
+        }
         lspconfig.clangd.setup({
             --name = 'clangd',
             ----           cmd = {'clangd', '--background-index', '--clang-tidy', '--log=verbose'},
