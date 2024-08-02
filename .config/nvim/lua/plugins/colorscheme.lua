b100d5a6c16fb3099c25c1f090dbeaea4715161b
sashan <sashang@gmail.com>
2024-08-02 10:31:21 +1000

    Update keymaps for telescope

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 return {
-    "folke/tokyonight.nvim", -- github name goes here: https://github.com/folke/tokyonight.nvim
+    "rebelot/kanagawa.nvim", -- github name goes here: https://github.com/folke/tokyonight.nvim
     priority = 1000,
     config = function()
-        vim.cmd([[colorscheme tokyonight]])
+        vim.cmd([[colorscheme kanagawa-dragon]])
     end
 }
