4745fc0dd70fee4aa36b1f999e4b3b6fe9c09afd
sashan <sashang@gmail.com>
2024-08-02 10:31:21 +1000

    Update keymaps for telescope

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,11 @@ keymap.set('n', '<leader>h', vim.lsp.buf.hover, {desc = 'hover tooltip'})
 keymap.set('n', 'c-]', vim.lsp.buf.definition, {desc = 'function definition'})
 keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {desc = 'function definition'})
 keymap.set('n', '<leader>z', '<cmd>Centerpad 80<cr>', {desc = 'center the buffer'})
+
+-- telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
