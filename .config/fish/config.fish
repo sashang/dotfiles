9b5829f131e09b8dc48b44f9f4a10c6e5052b5e0
sashan <sashang@gmail.com>
2024-11-30 17:05:03 +1100

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,3 @@
-set -x PATH "$HOME/.cargo/bin" "$HOME/.npm-packages/bin" "$HOME/dotnet" "$HOME/.dotnet/tools" "$HOME/.local/bin" "$HOME/bin" $PATH
+set -x PATH "$HOME/.cargo/bin" "$HOME/.npm-packages/bin" "$HOME/dotnet" "$HOME/.dotnet/tools" "$HOME/.local/bin" "$HOME/bin" "/mnt/c/Users/sashan/AppData/Local/Programs/cursor/resources/app/bin" $PATH 
 set -x EDITOR nvim
 
