c8ac05a4364131b239f8e9805419b775cdb83bfc
Sashan Govender <sashang@gmail.com>
2025-12-22 14:36:02 +1100

    update leaders

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,9 @@
 local keymap = vim.keymap
-keymap.set("n", "<localleader>h", ":set hlsearch!<cr>", {desc = "Turn off search highlights"})
-keymap.set("n", "<localleader>d", ":bd<cr>", {desc = "delete buffer"})
+keymap.set("n", "<leader>h", ":set hlsearch!<cr>", {desc = "Turn off search highlights"})
+keymap.set("n", "<leader>d", ":bd<cr>", {desc = "delete buffer"})
 keymap.set('n', '<leader>h', vim.lsp.buf.hover, {desc = 'hover tooltip'})
 keymap.set('n', 'c-]', vim.lsp.buf.definition, {desc = 'function definition'})
-keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {desc = 'function definition'})
+keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {desc = 'code action'})
 keymap.set('n', '<leader>z', '<cmd>Centerpad 80<cr>', {desc = 'center the buffer'})
 
 -- telescope
