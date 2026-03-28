b494c2608f5961c76abdf510fffc6caa6cd91b29
sashan <sashang@gmail.com>
2026-03-28 14:35:36 +1100

    Disable filewatching.
    Avoid the recursive stackoverflow. Roslyn tries to watch too many files.

--- .config/nvim/lua/plugins/roslyn.lua
+++ .config/nvim/lua/plugins/roslyn.lua
@@ -4,6 +4,12 @@ return {
   opts = {
     config = {
       capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      settings = {
+        ["csharp|background_analysis"] = {
+          dotnet_analyzer_diagnostics_scope = "openFiles",
+        },
+      },
     },
+    filewatching = "off",  -- disable Roslyn's internal file watcher, avoids a stack overflow when it recurses too deep.
   },
 }
