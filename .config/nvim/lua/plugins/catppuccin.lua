f2c77b498d541ae7aa17162824df9cb376258a12
sashan <sashang@gmail.com>
2026-06-07 22:21:11 +1000

    nvim: switch default theme to papercolor

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -2,7 +2,4 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    config = function()
-        vim.cmd "colorscheme catppuccin"
-    end
 }
---
+++ .config/nvim/lua/plugins/papercolor.lua
@@ -0,0 +1,8 @@
+return {
+    "nlknguyen/papercolor-theme",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.cmd("colorscheme PaperColor")
+    end,
+}
