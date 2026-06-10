12533395b745d2e2288e822d53fbb4302dd65ac3
sashan <sashang@gmail.com>
2026-06-10 13:23:12 +1000

    Fix F# LSP 100% CPU lockups by disabling semantic tokens

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -1,3 +1,4 @@
+vim.lsp.set_log_level("trace")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local rust_opts = require("lsp.servers.rust")
@@ -5,5 +6,16 @@ vim.lsp.config("rust_analyzer", vim.tbl_deep_extend("force", rust_opts, { capabi
 vim.lsp.config("clangd", { capabilities = capabilities })
 vim.lsp.config("mojo", { capabilities = capabilities })
 
-vim.lsp.enable({ "rust_analyzer", "clangd", "mojo" })
+vim.lsp.config("fsautocomplete", { 
+  capabilities = fsharp_capabilities,
+  on_init = function(client)
+    -- Disable semantic tokens to prevent 100% CPU lockups in the Neovim LSP worker.
+    -- fsautocomplete can send extremely large or overlapping token arrays in the background
+    -- (often triggered around the same time as a hover request), causing Neovim's internal
+    -- semantic token parser to infinite loop. We rely on Tree-sitter for highlighting instead.
+    client.server_capabilities.semanticTokensProvider = nil
+  end
+})
+
+vim.lsp.enable({ "rust_analyzer", "clangd", "mojo", "fsautocomplete", "lua_ls" })
 
--- .config/nvim/lua/plugins/ionide.lua
+++ .config/nvim/lua/plugins/ionide.lua
@@ -1,3 +1,4 @@
 return {
     "ionide/ionide-vim",
+    enabled = false,
 }
